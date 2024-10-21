5ebef72e8c8931fa2a7bb67c43e75c6935106e6d
ayesha786-dev <86068085+ayesha786-dev@users.noreply.github.com>
2024-10-21 15:19:53 +0530

    Create MaxUniqueSubstringsSplitter.java
    1593. Split a String Into the Max Number of Unique Substrings
    Medium LeetCode

---
+++ MaxUniqueSubstringsSplitter.java
@@ -0,0 +1,27 @@
+public class MaxUniqueSubstringsSplitter {
+
+	public int helper(String sub,Set<String> set) {
+		int max = 0;
+      for (int i = 1; i <= sub.length(); i++) {
+			
+			String subStr = sub.substring(0,i);
+			if(!set.contains(subStr)) {
+				set.add(subStr);
+				max = Math.max(max, 1+helper(sub.substring(i),set));
+				set.remove(subStr);
+			}
+		}
+      return max;
+	}
+    public int maxUniqueSplit(String s) {
+    	
+    
+		return helper(s,new HashSet<>());
+    }
+    
+    public static void main(String[] args) {
+		
+    	MaxUniqueSubstringsSplitter splitter= new MaxUniqueSubstringsSplitter();
+    	System.out.println(splitter.maxUniqueSplit("ababccc"));
+	}
+}
