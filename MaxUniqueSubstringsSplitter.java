4adda7e2a557a2d9ebc35005cc1edcc5e2e505a9
ayesha786-dev <86068085+ayesha786-dev@users.noreply.github.com>
2024-10-21 15:20:37 +0530

    Update MaxUniqueSubstringsSplitter.java

--- MaxUniqueSubstringsSplitter.java
+++ MaxUniqueSubstringsSplitter.java
@@ -1,27 +1,32 @@
+package com;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class MaxUniqueSubstringsSplitter {
 
-	public int helper(String sub,Set<String> set) {
+	public int helper(String sub, Set<String> set) {
 		int max = 0;
-      for (int i = 1; i <= sub.length(); i++) {
-			
-			String subStr = sub.substring(0,i);
-			if(!set.contains(subStr)) {
+		for (int i = 1; i <= sub.length(); i++) {
+
+			String subStr = sub.substring(0, i);
+			if (!set.contains(subStr)) {
 				set.add(subStr);
-				max = Math.max(max, 1+helper(sub.substring(i),set));
+				max = Math.max(max, 1 + helper(sub.substring(i), set));
 				set.remove(subStr);
 			}
 		}
-      return max;
+		return max;
 	}
-    public int maxUniqueSplit(String s) {
-    	
-    
-		return helper(s,new HashSet<>());
-    }
-    
-    public static void main(String[] args) {
-		
-    	MaxUniqueSubstringsSplitter splitter= new MaxUniqueSubstringsSplitter();
-    	System.out.println(splitter.maxUniqueSplit("ababccc"));
+
+	public int maxUniqueSplit(String s) {
+
+		return helper(s, new HashSet<>());
+	}
+
+	public static void main(String[] args) {
+
+		MaxUniqueSubstringsSplitter splitter = new MaxUniqueSubstringsSplitter();
+		System.out.println(splitter.maxUniqueSplit("ababccc"));
 	}
 }
